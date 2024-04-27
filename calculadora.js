d63a0a48ea05c712faf284a5d9e1c49503758510
Aluno Lab. 06 <aluno06@aeelabs.edu.br>
2024-04-26 21:29:11 -0300

    subindo código da calculadora imc

---
+++ calculadora.js
@@ -0,0 +1,7 @@
+function ola(){
+    let peso = document.getElementById('txtp')
+    let altura = document.getElementById('txta')
+    console.log('Olá')
+    let imc = (peso)/(altura*algura)
+    console.log('IMC')
+}
